789bf35443c5f758ff677d0c379445997561397c
FRC 2797 <10475967+FRC-2797@users.noreply.github.com>
2023-12-04 15:55:14 -0500

    Remove comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,12 +10,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 public class Robot extends TimedRobot {
   XboxController controller = new XboxController(0);
